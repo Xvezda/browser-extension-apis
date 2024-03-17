ccfb34456d544af803d579f8e60a737b28a4de57
Xvezda <xvezda@naver.com>
2024-03-17 18:50:16 +0900

    feat: add basic implementation

--- src/index.ts
+++ src/index.ts
@@ -25,8 +25,117 @@ export interface Env {
 	// MY_QUEUE: Queue;
 }
 
+function notFound() {
+	return Response.json({ status: 'error', value: 'Not Found' }, { status: 404 });
+}
+
+const stripPreamble = (text: string) => {
+	return text.replace(/^\)\]\}\'\,/, '');
+};
+
+const getPathsFromUrl = (url: string) => {
+	return new URL(url).pathname.substring(1).split('/');
+};
+
+const formatShieldsIo = (data: { version: string }) => {
+	return {
+		schemaVersion: 1,
+		label: 'version',
+		message: `v${data.version}`,
+	};
+};
+
+const stores = {
+	async fetch(request: Request, env: Env, ctx: ExecutionContext) {
+		const [_version, _api, target, ...paths] = getPathsFromUrl(request.url);
+
+		switch (target) {
+			case 'whale-store':
+				return whaleStore();
+			case 'edge-addons':
+				return edgeAddons();
+			default:
+				return notFound();
+		}
+
+		async function whaleStore() {
+			const [id, type] = paths;
+			const response = await fetch(`https://store.whale.naver.com/ajax/extensions/${id}`, {
+				headers: {
+					Accept: 'application/json, text/plain, */*',
+					'X-Requested-With': 'XMLHttpRequest',
+					'User-Agent':
+						'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.159 Safari/537.36',
+				},
+			});
+			const text = await response.text();
+			const json = JSON.parse(stripPreamble(text));
+
+			switch (type) {
+				case 'raw':
+					return Response.json(json);
+				case 'version':
+					if (new URL(request.url).searchParams.get('format') === 'shields-io') {
+						return Response.json(formatShieldsIo(json));
+					}
+					return Response.json({ version: json.version });
+				default:
+					return notFound();
+			}
+		}
+
+		async function edgeAddons() {
+			const [id, type] = paths;
+			const response = await fetch(`https://microsoftedge.microsoft.com/addons/getproductdetailsbycrxid/${id}`, {
+				headers: {
+					Accept: 'application/json, text/plain, */*',
+					Referer: `https://microsoftedge.microsoft.com/addons/detail/${id}`,
+					'User-Agent':
+						'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.159 Safari/537.36',
+				},
+			});
+			const text = await response.text();
+			const json = JSON.parse(stripPreamble(text));
+
+			switch (type) {
+				case 'raw':
+					return Response.json(json);
+				case 'version':
+					if (new URL(request.url).searchParams.get('format') === 'shields-io') {
+						return Response.json(formatShieldsIo(json));
+					}
+					return Response.json({ version: json.version });
+				default:
+					return notFound();
+			}
+		}
+	},
+};
+
+const handlers = {
+	async fetch(request: Request, env: Env, ctx: ExecutionContext) {
+		const [_version, api] = getPathsFromUrl(request.url);
+
+		switch (api) {
+			case 'stores':
+				return await stores.fetch(request, env, ctx);
+			default:
+				return notFound();
+		}
+	},
+};
+
 export default {
 	async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
-		return new Response('Hello World!');
+		try {
+			const [version] = getPathsFromUrl(request.url);
+
+			if (version !== 'v1') return notFound();
+
+			return await handlers.fetch(request, env, ctx);
+		} catch (e) {
+			const error = e as Error;
+			return Response.json({ message: error.message }, { status: 500 });
+		}
 	},
 };
